463be5b2b8c8654a3ec38e1a2c17205484ed7de6
ginbear <r.shimizu47@gmail.com>
2015-11-12 17:51:51 +0900

    update

--- bin/setup_mac_homebrew.sh
+++ bin/setup_mac_homebrew.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-## 基本tool
+## basic tools
 brew install wget
 brew install nkf
 brew install watch
 brew install tree
 brew install tmux
 brew install ag
+brew install trash
 
-## 運用tool
+## ops tools
 brew install ansible
 brew install gist
 brew install tig
-brew install Caskroom/cask/java  ## jenkins 依存
-brew install jenkin
+brew install fabric
+
+# jenkins
+# brew install Caskroom/cask/java
+# brew install jenkin
 # To have launchd start jenkins at login:
 #     ln -sfv /usr/local/opt/jenkins/*.plist ~/Library/LaunchAgents
 # Then to load jenkins now:
 #     launchctl load ~/Library/LaunchAgents/homebrew.mxcl.jenkins.plist
 # Or, if you don't want/need launchctl, you can just run:
 #     jenkinss
-brew install fabric
-
-## 役立ち
-brew install trash
