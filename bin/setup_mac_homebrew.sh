c1de5801730df1e7cca4d01f5b1bd519e0fde4b0
ginbear <r.shimizu47@gmail.com>
2015-11-12 19:13:38 +0900

    update

--- bin/setup_mac_homebrew.sh
+++ bin/setup_mac_homebrew.sh
@@ -2,18 +2,30 @@
 
 ## basic tools
 brew install wget
+brew install htop
 brew install nkf
 brew install watch
 brew install tree
-brew install tmux
 brew install ag
 brew install trash
+brew install coreutils
+brew tap peco/peco
+brew install peco
 
 ## ops tools
-brew install ansible
+# brew install ansible
+brew install tmux
 brew install gist
 brew install tig
 brew install fabric
+
+## ricty
+brew tap sanemat/font
+brew install Caskroom/cask/xquartz
+brew install ricty
+
+cp -f /usr/local/opt/ricty/share/fonts/Ricty*.ttf ~/Library/Fonts/
+fc-cache -vf
 
 # jenkins
 # brew install Caskroom/cask/java
