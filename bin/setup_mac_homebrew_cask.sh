212fadf25fd4bdcd595b585ace8399794b93369b
tora <shimizu@pepabo.com>
2017-08-29 20:55:47 +0900

    homebew cask で install する対象を整理

--- bin/setup_mac_homebrew_cask.sh
+++ bin/setup_mac_homebrew_cask.sh
@@ -2,24 +2,51 @@
 
 brew tap caskroom/cask
 
-brew cask install google-japanese-ime
+# browser
 brew cask install firefox
 brew cask install google-chrome
+
+# editor
 brew cask install macvim
+brew cask install boostnote
+
+# terminal
 brew cask install iterm2
-brew cask install totalterminal
+
+# productivity
 brew cask install karabiner
 brew cask install spectacle
 brew cask install sourcetree
-brew cask install sequel-pro 
-brew cask install dash
+brew cask install gitify
+
+# dev
 brew cask install virtualbox
 brew cask install vagrant
+brew cask install docker
+
+# communication
+brew cask install skype
 brew cask install slack
-brew cask install cyberduck
-brew cask install skitch
+brew cask install franz
 brew cask install tunnelblick
+
+# tools
+brew cask install the-unarchiver
+brew cask install skitch
+brew cask install cyberduck
+brew cask install sequel-pro 
+
+# other
+brew cask install kindle
+
+# brew cask install google-japanese-ime
+# brew cask install totalterminal
+# brew cask install dash
 # brew cask install keepassx
 # brew cask install cheatsheet
 # brew cask install vlc
 # brew cask install evernote
+
+## 以下のアプリは手動で入れる必要がありそう
+# alfred3
+# mindnode
