178591754aa1dd9cacfe8e4e41438cdf58592f78
tora <shimizu@pepabo.com>
2017-09-26 21:08:03 +0900

    Mac OS の update に伴い openstack コマンド方法を update

--- bin/setup_openstack.sh
+++ bin/setup_openstack.sh
@@ -1,8 +1,12 @@
 # pyenv が必要なので先に `bin/setup_mac_homebrew.sh` を実行する
 
-pyenv install 2.7.10
-pyenv global 2.7.10
-pip install -U pip
+PYVERSION="2.7.14"
 
-brew install pyenv
+# https://github.com/pyenv/pyenv/wiki/Common-build-problems
+# High Sierra にしてから標準の openssl が LibreSSL になったので
+CFLAGS="-I$(brew --prefix openssl)/include" \
+LDFLAGS="-L$(brew --prefix openssl)/lib" \
+pyenv install -v ${PYVERSION}
+pyenv global ${PYVERSION}
+pip install -U pip
 pip install python-openstackclient python-neutronclient python-novaclient
