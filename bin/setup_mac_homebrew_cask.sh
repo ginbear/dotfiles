69d56dd3e7da6e6cbd36d442d3ee4f30e0ac4848
ginbear <shimizu@pepabo.com>
2025-05-15 15:26:43 +0900

    Update setup_mac_homebrew_cask.sh

--- bin/setup_mac_homebrew_cask.sh
+++ bin/setup_mac_homebrew_cask.sh
@@ -4,35 +4,26 @@ brew tap homebrew/cask
 
 # browser
 brew install firefox --cask
-brew install brave-browser --cask
 
 # editor
 brew install macvim --cask
 brew install visual-studio-code --cask
 
-# terminal
-brew install iterm2 --cask
-brew install warp --cask
-
 # productivity
 brew install karabiner-elements --cask
-brew install spectacle --cask
 brew install bartender --cask
 
 # dev
-brew install docker --cask
-brew install google-cloud-sdk --cask
+# brew install docker --cask
+# brew install google-cloud-sdk --cask
 
 # communication
-brew install slack --cask
+# brew install slack --cask
 
 # tools
 brew install the-unarchiver --cask
 brew install skitch --cask
 brew install notion --cask
-brew install alfred --cask
-brew install 1password --cask
-brew install 1password-cli --cask
 
 # other
 brew install kindle --cask
